510fe0edbdc0bd273cf7995c4557d7463bc64f5c
Brian Turchyn <brian@dj-bri-t.net>
2016-03-21 22:13:19 -0500

    Move more logic into window files

--- app/windows/loginWindow.js
+++ app/windows/loginWindow.js
@@ -1,15 +1,21 @@
 
 import { app, BrowserWindow } from 'electron';
-import devHelper from './vendor/electron_boilerplate/dev_helper';
+import devHelper from '../vendor/electron_boilerplate/dev_helper';
+import env from '../env';
 
 export default function() {
-  result = new BrowserWindow({
+  var result = new BrowserWindow({
     width: 345,
     height: 500,
     frame: false
   });
 
   result.loadURL('file://' + __dirname + '/login.html');
+
+  if (env.name !== 'production') {
+    devHelper.setDevMenu();
+    result.openDevTools();
+  }
 
   return result;
 };
--- app/windows/mainWindow.js
+++ app/windows/mainWindow.js
@@ -1,17 +1,18 @@
 
 import { app, BrowserWindow } from 'electron';
-import devHelper from './vendor/electron_boilerplate/dev_helper';
-import windowStateKeeper from './vendor/electron_boilerplate/window_state';
+import devHelper from '../vendor/electron_boilerplate/dev_helper';
+import windowStateKeeper from '../vendor/electron_boilerplate/window_state';
 
 export default function() {
-  mainWindowState = windowStateKeeper('main', {
+  var mainWindowState = windowStateKeeper('main', {
     width: 1000,
     height: 600
   });
 
-  result = new BrowserWindow({
+  var result = new BrowserWindow({
     width: 1000,
-    height: 600
+    height: 600,
+    show: false
   });
 
   if (mainWindowState.isMaximized) {
@@ -19,10 +20,6 @@ export default function() {
   }
 
   result.loadURL('file://' + __dirname + '/app.html');
-
-  result.on('close', function () {
-    mainWindowState.saveState(mainWindow);
-  });
 
   return result;
 };
