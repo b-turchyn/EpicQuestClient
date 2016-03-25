3f00a3d6a48c331547e221771b3116f74a916a8c
Brian Turchyn <brian@dj-bri-t.net>
2016-03-25 13:57:13 -0500

    Fixes #3; global require issue

--- app/env.js
+++ app/env.js
@@ -1,13 +1,11 @@
 // Simple module exposes environment variables to rest of the code.
-
 import jetpack from 'fs-jetpack';
+var app = (
+    process.type === 'renderer' ? 
+    require('electron').remote.app : 
+    require('electron').app
+  );
 
-var app;
-if (process.type === 'renderer') {
-    app = require('electron').remote.app;
-} else {
-    app = require('electron').app;
-}
 var appDir = jetpack.cwd(app.getAppPath());
 
 var manifest = appDir.read('package.json', 'json');
